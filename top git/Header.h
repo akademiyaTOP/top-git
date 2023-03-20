0c088b347ebe621e50d4e9987e7212db66a6883c
akademiyaTOP <dk26.01.1999@yandex.ru>
2023-03-20 21:14:00 +0300

    Улучшили версию предыдущей программу, добавив новую переменную

--- top git/Header.h
+++ top git/Header.h
@@ -2,7 +2,7 @@
 #include <iostream>
 using namespace std;
 
-void function(int a)
+void function(int a, const int b)
 {
 	for (int i(0); i < a; ++i)
 	cout << "Hi  ";
--- top git/Source.cpp
+++ top git/Source.cpp
@@ -4,15 +4,16 @@
 int main()
 {
 	int a(5);
+	const int b(3);
 	std::cout << a << std::endl;
 	std::cout << "Hallo World\n";
 
 
-	function(a);
+	function(a, b);
 
-	std::cout << a << std::endl;
+	std::cout << a << "  " << b << std::endl;
 	std::cout << "Hallo\n";
 
-	function(a);
+	function(a, b);
 
 }
